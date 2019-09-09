dcdb68158d6fddecf157eafefcb2cb9e3b271107
qiluge <wangcheng@onchain.com>
2019-09-09 11:28:27 +0800

    refactor imports file

--- ont_sdk.go
+++ ont_sdk.go
@@ -22,23 +22,22 @@ package ontology_go_sdk
 import (
 	"encoding/hex"
 	"fmt"
-	"github.com/ontio/go-bip32"
-	"github.com/ontio/ontology-go-sdk/bip44"
-	"github.com/ontio/ontology/smartcontract/event"
-	"github.com/tyler-smith/go-bip39"
 	"io"
 	"math/rand"
 	"time"
 
+	"github.com/ontio/go-bip32"
 	"github.com/ontio/ontology-crypto/keypair"
+	"github.com/ontio/ontology-go-sdk/bip44"
 	"github.com/ontio/ontology-go-sdk/client"
-	common3 "github.com/ontio/ontology-go-sdk/common"
+	sdkComm "github.com/ontio/ontology-go-sdk/common"
 	"github.com/ontio/ontology-go-sdk/utils"
 	"github.com/ontio/ontology/common"
-	common2 "github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/common/constants"
 	"github.com/ontio/ontology/core/payload"
 	"github.com/ontio/ontology/core/types"
+	"github.com/ontio/ontology/smartcontract/event"
+	"github.com/tyler-smith/go-bip39"
 )
 
 func init() {
@@ -99,7 +98,7 @@ func ParsePayload(code []byte) (map[string]interface{}, error) {
 		// +1   length
 		//TODO if version>15, there will be bug
 		if l > 54 && string(code[l-46-8:l-46]) == "transfer" {
-			param := make([]common3.StateInfo, 0)
+			param := make([]sdkComm.StateInfo, 0)
 			source := common.NewZeroCopySource(code)
 			for {
 				zeroByte, eof := source.NextByte()
@@ -133,7 +132,7 @@ func ParsePayload(code []byte) (map[string]interface{}, error) {
 				if err != nil {
 					return nil, err
 				}
-				state := common3.StateInfo{
+				state := sdkComm.StateInfo{
 					From:  from.ToBase58(),
 					To:    to.ToBase58(),
 					Value: amount,
@@ -214,7 +213,7 @@ func ParsePayload(code []byte) (map[string]interface{}, error) {
 			if err != nil {
 				return nil, err
 			}
-			tf := common3.TransferFromInfo{
+			tf := sdkComm.TransferFromInfo{
 				Sender: sender.ToBase58(),
 				From:   from.ToBase58(),
 				To:     to.ToBase58(),
@@ -291,7 +290,7 @@ func isEnd(source *common.ZeroCopySource) (bool, error) {
 	}
 }
 
-func readAddress(source *common.ZeroCopySource) (common2.Address, error) {
+func readAddress(source *common.ZeroCopySource) (common.Address, error) {
 	senderBytes, _, irregular, eof := source.NextVarBytes()
 	if irregular || eof {
 		return common.ADDRESS_EMPTY, io.ErrUnexpectedEOF
@@ -463,7 +462,7 @@ func (this *OntologySdk) GetTxData(tx *types.MutableTransaction) (string, error)
 	if err != nil {
 		return "", fmt.Errorf("IntoImmutable error:%s", err)
 	}
-	sink := common2.ZeroCopySink{}
+	sink := common.ZeroCopySink{}
 	txData.Serialization(&sink)
 	rawtx := hex.EncodeToString(sink.Bytes())
 	return rawtx, nil
